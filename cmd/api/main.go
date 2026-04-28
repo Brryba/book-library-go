a422a2d36ef2c62cf3eec83d7ead733753716a0e
Yury Brazgunou <jurabrezgunov@gmail.com>
2026-04-28 19:53:16 +0200

    add main function + router

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,41 @@
 package main
 
-func main() {
+import (
+	"context"
+	"log"
+	"net/http"
 
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/jackc/pgx/v5/pgxpool"
+
+	"book-library-go/config"
+	"book-library-go/internal/author"
+)
+
+func main() {
+	cfg := config.Load()
+
+	db, err := pgxpool.New(context.Background(), cfg.DatabaseURL)
+	if err != nil {
+		log.Fatalf("failed to connect to database: %v", err)
+	}
+	defer db.Close()
+
+	if err := db.Ping(context.Background()); err != nil {
+		log.Fatalf("failed to ping database: %v", err)
+	}
+
+	authorRepo := author.NewRepository(db)
+	authorService := author.NewService(authorRepo)
+	authorHandler := author.NewHandler(authorService)
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Route("/authors", authorHandler.Routes)
+
+	log.Printf("server started on port %s", cfg.Port)
+	log.Fatal(http.ListenAndServe(":"+cfg.Port, r))
 }
