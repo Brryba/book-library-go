7854344b88553bd85d038a970f6f6c9838ed9bea
Yury Brazgunou <jurabrezgunov@gmail.com>
2026-04-28 19:35:06 +0200

    add author service

--- internal/author/service.go
+++ internal/author/service.go
@@ -1,1 +1,66 @@
 package author
+
+import (
+	"context"
+	"errors"
+
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5"
+)
+
+var ErrNotFound = errors.New("author not found")
+
+type Service struct {
+	repo *Repository
+}
+
+func NewService(repo *Repository) *Service {
+	return &Service{repo: repo}
+}
+
+func (s *Service) GetAll(ctx context.Context) ([]Author, error) {
+	return s.repo.FindAll(ctx)
+}
+
+func (s *Service) GetByID(ctx context.Context, id uuid.UUID) (*Author, error) {
+	a, err := s.repo.FindByID(ctx, id)
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return nil, ErrNotFound
+		}
+		return nil, err
+	}
+	return a, nil
+}
+
+func (s *Service) Create(ctx context.Context, req CreateRequest) (*Author, error) {
+	if req.Name == "" {
+		return nil, errors.New("name is required")
+	}
+	return s.repo.Create(ctx, req)
+}
+
+func (s *Service) Update(ctx context.Context, id uuid.UUID, req UpdateRequest) (*Author, error) {
+	if req.Name == "" {
+		return nil, errors.New("name is required")
+	}
+	_, err := s.repo.FindByID(ctx, id)
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return nil, ErrNotFound
+		}
+		return nil, err
+	}
+	return s.repo.Update(ctx, id, req)
+}
+
+func (s *Service) Delete(ctx context.Context, id uuid.UUID) error {
+	_, err := s.repo.FindByID(ctx, id)
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return ErrNotFound
+		}
+		return err
+	}
+	return s.repo.Delete(ctx, id)
+}
